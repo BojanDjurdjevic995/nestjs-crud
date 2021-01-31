1c1ed7eaae249d92cc644236b5876cb2f885eb31
Bojan Djurdjevic <djurdjevicbojan12@gmail.com>
2021-01-31 21:27:41 +0100

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,11 +17,11 @@ import { AuthModule } from './auth/auth.module';
     UserModule,
     TypeOrmModule.forRoot({
       type : "mysql",
-      host : "localhost",
-      port : 3306,
-      username : "bojan",
-      password : "pecenegilova12",
-      database : "node_js_tut",
+      host : process.env.DB_HOST,
+      port : Number(process.env.DB_PORT),
+      username : process.env.DB_USERNAME,
+      password : process.env.DB_PASSWORD,
+      database : process.env.DB_DATABASE,
       entities : [NewsEntity, TaskEntity, UserEntity],
       // synchronize : true
     }),
